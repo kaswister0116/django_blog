0452967f54c925d3dee1b28f2617ced7b8e96243
Kaoru <kaswister0116@gmail.com>
2024-02-09 23:47:10 +0900

    Initial Commit

---
+++ techpit/blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from blog.models import Category, Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display=('id','title','postdate','category')
+
+
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
